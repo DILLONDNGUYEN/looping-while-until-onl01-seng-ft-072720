a6dfc92ed9ae2e1ef38d2bab2299a66f7abe90d3
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-29 05:19:17 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -4,6 +4,8 @@ def using_until(force)
   until levitation_force == 10
   #puts the desired phrase, "Wingardium Leviosa"
   puts "Wingardium Leviosa"
+  #increment your levitation force by 1
+  levitation_force += 1
   #your code here
     
 end
